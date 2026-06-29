8fc8a45ddf42ee487dc0b838e83d4340969829f1
Anish3627 <anishkumar639421@gmail.com>
2026-06-29 16:38:21 +0530

    dependency fix

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/collegeDB");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("✅ MongoDB Connected");
+    console.log(`✅ MongoDB Atlas Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error);
+    console.error("❌ MongoDB Connection Error:", error.message);
     process.exit(1);
   }
 };
